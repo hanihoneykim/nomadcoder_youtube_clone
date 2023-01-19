8bdf9fa250de74f8cbdc86e91866eba09231450c
hanihoneykim <107341819+hanihoneykim@users.noreply.github.com>
2023-01-20 02:52:25 +0900

    #13.1-#13.4 Video Preview, Recording, Downloading 강의 내용

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,53 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
-const handleStart = async() => {
-    const stream = await navigator.mediaDevices.getUserMedia({
-        audio:true,
-        video:false,
-    })
-    console.log(stream)
+let stream;
+let recorder;
+let videoFile;
+
+const handleDownload = () => {
+    const a = document.createElement("a")
+    a.href = videoFile;
+    a.download = "MyRecording.webm";
+    document.body.appendChild(a);
+    a.click();
+    const tracks = stream.getTracks();
+    tracks.forEach((track) => {
+        track.stop();
+    });
+    stream = null;
 }
+
+const handleStop = () => {
+    startBtn.innerText = "Download Recording";
+    startBtn.removeEventListener("click", handleStop);
+    startBtn.addEventListener("click", handleDownload);
+    recorder.stop();
+}
+
+const handleStart = () => {
+    startBtn.innerText = "Stop Recording";
+    startBtn.removeEventListener("click", handleStart);
+    startBtn.addEventListener("click", handleStop);
+    recorder = new window.MediaRecorder(stream)
+    recorder.ondataavailable = (event) => {
+        videoFile = URL.createObjectURL(event.data);
+        video.srcObject = null;
+        video.src = videoFile;
+        video.play();
+    }
+    recorder.start();
+}
+
+const init = async() => {
+    stream = await navigator.mediaDevices.getUserMedia({
+        audio:true,
+        video:true,
+    })
+    video.srcObject = stream;   //pug에 source 없는 video 만들고, srcObject에 stream 넣어준다.
+    video.play();
+}
+
+init();
 
 startBtn.addEventListener("click", handleStart);
